c6a10a17d910ff4e8d6424fc15d5d6115c890785
jchin14 <jchin1408@gmail.com>
2024-12-02 17:18:39 +1100

    Day 2

---
+++ 2.py
@@ -0,0 +1,51 @@
+import csv
+
+reports = []
+
+with open('2.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        reports.append(row)
+
+def checkReport(report):
+    report = [int(i) for i in report]
+
+    for i in range(len(report)):
+        if i == len(report) - 1:
+            return 1
+        if abs(report[i] - report[i + 1]) > 3 or abs(report[i] - report[i + 1]) < 1:
+            return 0
+        
+        if report[i] < report[i + 1]:
+            for j in range(len(report)):
+                if j == len(report) - 1:
+                    pass
+                elif report[j] > report[j+1]:
+                    return 0
+        if report[i] > report[i + 1]:
+            for j in range(len(report)):
+                if j == len(report) - 1:
+                    pass
+                elif report[j] < report[j+1]:
+                    return 0
+    return 1
+
+safeCount = 0
+
+for report in reports:
+    copyReport = report[:]
+    if checkReport(report) == 0:
+        print(report)
+        for i in range(len(report)):
+            newReport = copyReport[:]
+            newReport.pop(i)
+            print(newReport)
+            if checkReport(newReport) == 1:
+                safeCount += 1
+                break
+        print("-----\n")
+    else:
+        safeCount += 1
+
+
+print(safeCount)
